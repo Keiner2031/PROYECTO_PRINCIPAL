8a6ed364877ef9356100b068874120ccc20291b9
Keiner2031 <137812906+Keiner2031@users.noreply.github.com>
2024-05-27 19:01:02 -0500

    primer commit

---
+++ vistas/contenidos/es/web-index.php
@@ -0,0 +1,49 @@
+<div class="banner">
+    <div class="banner-body">
+        <h3 class="text-uppercase">Bienvenido a <?php echo COMPANY; ?></h3>
+        <p>Los mejores productos y la mejor calidad los encuentras en <?php echo COMPANY; ?></p>
+    </div>
+</div>
+
+<div class="container-fluid container-web-page border-bottom bg-white">
+    <h3 class="text-center text-uppercase poppins-regular font-weight-bold">Nuestros servicios</h3>
+    <br>
+    <div class="row">
+        <div class="col-12 col-sm-6 col-md-4">
+            <p class="text-center"><i class="fas fa-shipping-fast fa-5x"></i></p>
+            <h5 class="text-center text-uppercase poppins-regular font-weight-bold">Envíos a domicilio</h5>
+            <p class="text-center">¡Bienvenido a Ferretería FV SAS, tu solución integral para proyectos de construcción y mejoras para el hogar! Ahora, con nuestro servicio de ENVÍOS A DOMICILIO, llevamos la comodidad directamente a tu puerta.</p>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4">
+            <p class="text-center"><i class="fas fa-boxes fa-5x"></i></p>
+            <h5 class="text-center text-uppercase poppins-regular font-weight-bold">Ventas al por mayor</h5>
+            <p class="text-center">Descubre nuestras ofertas exclusivas para compras al por mayor. Con un amplio catálogo y precios competitivos, somos tu socio confiable para abastecer tus necesidades comerciales a gran escala. ¡Haz crecer tu negocio con nosotros hoy mismo!</p>
+        </div>
+        <div class="col-12 col-sm-6 col-md-4">
+            <p class="text-center"><i class="fas fa-store-alt fa-5x"></i></p>
+            <h5 class="text-center text-uppercase poppins-regular font-weight-bold">Retiro en tienda</h5>
+            <p class="text-center">¿Prefieres recoger tu pedido en nuestra tienda? ¡Con gusto! Con nuestro servicio de Retiro en Tienda, puedes realizar tus compras en línea y recogerlas personalmente en nuestra ubicación. Es fácil y conveniente. Solo realiza tu pedido en línea, selecciona la opción de Retiro en Tienda durante el proceso de compra y te avisaremos cuando esté listo para que lo recojas. ¡Así de simple!</p>
+        </div>
+    </div>
+</div>
+
+
+<div class="container container-web-page">
+    <div class="row justify-content-md-center">
+        <div class="col-12 col-md-6">
+            <figure class="full-box">
+                <img src="<?php echo SERVERURL; ?>vistas/assets/img/registration.png" alt="registration_<?php echo COMPANY; ?>" class="img-fluid">
+            </figure>
+        </div>
+        <div class="w-100"></div>
+        <div class="col-12 col-md-6">
+            <h3 class="text-center text-uppercase poppins-regular font-weight-bold">Crea tu cuenta</h3>
+            <p class="text-justify">
+                Crea tu cuenta para poder realizar pedidos de productos hasta la puesta de tu casa, es muy fácil y rápido.
+            </p>
+            <p class="text-center">
+                <a href="<?php echo SERVERURL; ?>registration/" class="btn btn-primary" >Crear cuenta</a>
+            </p>
+        </div>
+    </div>
+</div>
